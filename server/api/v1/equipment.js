892483c7764de7a2e59739aaf5c583e74194b6bb
ywais <yuvalwais@gmail.com>
2021-01-14 14:19:01 +0200

    get equipment by location

--- server/api/v1/equipment.js
+++ server/api/v1/equipment.js
@@ -25,4 +25,21 @@ mysqlCon.connect(error => {
 router.get('/', (req, res) => {
 });
 
+router.get('/location/:id', (req, res) => {
+  try {
+    mysqlCon.query(`SELECT equ_name as 'שם', eil_quantity as 'כמות', eil_notes as 'הערות'
+    FROM sql_derech_haketzev.equipment_in_locations
+    INNER JOIN sql_derech_haketzev.locations ON eil_location_id = loc_id
+    INNER JOIN sql_derech_haketzev.equipment ON eil_equipment_id = equ_id
+    INNER JOIN sql_derech_haketzev.equipment_types ON equ_type = eqt_id
+    WHERE loc_id = ${req.params.id};`, (error, results, fields) => {
+      if(error) throw error;
+      res.send(results);
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ message: 'Cannot process request' });
+  }
+});
+
 module.exports = router;
